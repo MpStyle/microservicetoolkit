56a651a9eab9000888a1a0024096689808f2e3ab
Michele Pagnin <michele.pagnin@carel.com>
2021-03-02 15:17:44 +0100

    Added support for Redis cache

---
+++ microservicetoolkit/book/DateTimeUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace mpstyle.microservice.toolkit.book
+{
+    public class DateTimeUtils
+    {
+        /// <summary>
+        /// Uses and requires UTC timezone.
+        /// </summary>
+        /// <param name="unixTimeStamp"></param>
+        /// <returns></returns>
+        public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+        {
+            var toReturn = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(unixTimeStamp);
+            return toReturn;
+        }
+    }
+}
---
+++ microservicetoolkit/book/cachemanager/RedisCacheManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+
+using StackExchange.Redis;
+
+using System.Threading.Tasks;
+
+namespace mpstyle.microservice.toolkit.book.cachemanager
+{
+    public class RedisCacheManager : Disposable, ICacheManager
+    {
+        private readonly ILogger<RedisCacheManager> logger;
+        private readonly ConnectionMultiplexer connection;
+
+        public RedisCacheManager(string connectionString, ILogger<RedisCacheManager> logger)
+        {
+            this.connection = ConnectionMultiplexer.Connect(connectionString);
+            this.logger = logger;
+        }
+
+        public async Task<string> Get(string key)
+        {
+            this.logger.LogDebug($"Calling RedisCacheManager#Get({key ?? string.Empty})...");
+            var db = this.connection.GetDatabase();
+            var result = await db.StringGetWithExpiryAsync(key);
+
+            if (result.Expiry.HasValue && result.Expiry.Value.TotalMilliseconds > 0 && result.Value.HasValue)
+            {
+                return result.Value.ToString();
+            }
+
+            return null;
+        }
+
+        public async Task<bool> Set(string key, string value, long issuedAt)
+        {
+            this.logger.LogDebug($"Calling RedisCacheManager#Set({key ?? string.Empty})...");
+            var db = this.connection.GetDatabase();
+            var setResult = await db.StringSetAsync(key, value);
+
+            if (setResult)
+            {
+                return await db.KeyExpireAsync(key, DateTimeUtils.UnixTimeStampToDateTime(issuedAt));
+            }
+
+            return setResult;
+        }
+    }
+}
