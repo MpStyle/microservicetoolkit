e293cc640b0c1873201738fc8cbb4dcd21345926
Michele Pagnin <michele.pagnin@carel.com>
2021-09-07 08:40:10 +0200

    Changed error codes values

--- microservicetoolkit/book/ErrorCode.cs
+++ microservicetoolkit/book/ErrorCode.cs
@@ -2,10 +2,11 @@
 {
     public static class ErrorCode
     {
-        public const int UNKNOWN = 0;
-        public const int SERVICE_NOT_FOUND = 1;
-        public const int INVALID_SERVICE = 2;
-        public const int INVALID_SERVICE_CALL = 3;
-        public const int INVALID_SERVICE_EXECUTION = 4;
+        public const int UNKNOWN = 8000;
+        public const int SERVICE_NOT_FOUND = 8001;
+        public const int INVALID_SERVICE = 8002;
+        public const int INVALID_SERVICE_CALL = 8003;
+        public const int INVALID_SERVICE_EXECUTION = 8004;
+        public const int INVALID_SERVICE_RESPONSE = 8005;
     }
 }
