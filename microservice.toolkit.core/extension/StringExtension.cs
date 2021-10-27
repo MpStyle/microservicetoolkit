99330d0608267dad5bc9d16565f1b326bfb5808a
Michele Pagnin <MpStyle@users.noreply.github.com>
2021-10-27 14:53:50 +0200

    Created "IsNullOrEmpty" string extension

---
+++ microservice.toolkit.core/extension/StringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace microservice.toolkit.core.extension
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
