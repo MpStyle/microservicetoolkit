dc91e5b3d10a2ac1748235a05f9a226d1442b096
Michele Pagnin <MpStyle@users.noreply.github.com>
2022-11-21 17:20:15 +0100

    Added creation timestamp in brokered event

--- microservice.toolkit.messagemediator/entity/BrokeredEvent.cs
+++ microservice.toolkit.messagemediator/entity/BrokeredEvent.cs
@@ -1,9 +1,20 @@
-﻿namespace microservice.toolkit.messagemediator.entity
+﻿using System;
+
+namespace microservice.toolkit.messagemediator.entity
 {
     internal class BrokeredEvent
     {
+        public BrokeredEvent() {
+            this.Created = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+
         public string Pattern { get; init; }
         public object Payload { get; init; }
         public string RequestType { get; init; }
+
+        /// <summary>
+        /// Timestamp in milliseconds of creation date
+        /// </summary>
+        public long Created { get; }
     }
 }
