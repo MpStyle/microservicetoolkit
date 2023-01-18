c925e564897576025518b82b5fc7aa2d2f7dc5f9
Michele Pagnin <MpStyle@users.noreply.github.com>
2023-01-18 11:58:46 +0100

    Added extension method to call a service by type and response

--- microservice.toolkit.core/extension/MessageMediatorExtensions.cs
+++ microservice.toolkit.core/extension/MessageMediatorExtensions.cs
@@ -7,7 +7,14 @@ namespace microservice.toolkit.core.extension;
 
 public static class MessageMediatorExtensions
 {
-    public static async Task<ServiceResponse<TPayload>> Send<TRequest, TPayload>(this IMessageMediator messageMediator, Type serviceType, TRequest message)
+    public static async Task<ServiceResponse<TPayload>> Send<TRequest, TPayload>(this IMessageMediator messageMediator,
+        Type serviceType, TRequest message)
+    {
+        return await messageMediator.Send<TRequest, TPayload>(serviceType.ToPattern(), message);
+    }
+
+    public static async Task<ServiceResponse<TPayload>> Send<TPayload>(this IMessageMediator messageMediator,
+        Type serviceType, object message)
     {
         return await messageMediator.Send<TPayload>(serviceType.ToPattern(), message);
     }
@@ -21,7 +28,8 @@ public static class MessageMediatorExtensions
     /// <param name="pattern"></param>
     /// <param name="message"></param>
     /// <returns></returns>
-    public static async Task<ServiceResponse<TPayload>> Send<TRequest, TPayload>(this IMessageMediator messageMediator, string pattern, TRequest message)
+    public static async Task<ServiceResponse<TPayload>> Send<TRequest, TPayload>(this IMessageMediator messageMediator,
+        string pattern, TRequest message)
     {
         return await messageMediator.Send<TPayload>(pattern, message);
     }
