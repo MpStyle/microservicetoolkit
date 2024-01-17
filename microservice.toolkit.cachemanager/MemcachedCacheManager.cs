7d0c4c6d2d0e6c94c74a0906dd2d72240afd4bc0
Michele Pagnin <MpStyle@users.noreply.github.com>
2024-01-17 17:19:36 +0100

    File scoped namespace in MemcachedCacheManager class

--- microservice.toolkit.cachemanager/MemcachedCacheManager.cs
+++ microservice.toolkit.cachemanager/MemcachedCacheManager.cs
@@ -5,51 +5,50 @@ using microservice.toolkit.core;
 using System;
 using System.Threading.Tasks;
 
-namespace microservice.toolkit.cachemanager
+namespace microservice.toolkit.cachemanager;
+
+public class MemcachedCacheManager : Disposable, ICacheManager
 {
-    public class MemcachedCacheManager : Disposable, ICacheManager
+    private readonly IMemcachedClient client;
+
+    public MemcachedCacheManager(IMemcachedClient client)
     {
-        private readonly IMemcachedClient client;
+        this.client = client;
+    }
 
-        public MemcachedCacheManager(IMemcachedClient client)
+    public Task<bool> Delete(string key)
+    {
+        return this.client.RemoveAsync(key);
+    }
+
+    public async Task<TValue> Get<TValue>(string key)
+    {
+        var result = await this.client.GetAsync<TValue>(key);
+
+        return result.HasValue ? result.Value : default;
+    }
+
+    public Task<bool> Set<TValue>(string key, TValue value, long issuedAt)
+    {
+        if (issuedAt != 0 && issuedAt < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
         {
-            this.client = client;
+            this.Delete(key);
+            return Task.FromResult(false);
         }
 
-        public Task<bool> Delete(string key)
-        {
-            return this.client.RemoveAsync(key);
-        }
+        var duration = (int)((issuedAt - DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()) / 1000);
 
-        public async Task<TValue> Get<TValue>(string key)
-        {
-            var result = await this.client.GetAsync<TValue>(key);
+        return this.client.SetAsync(key, value, duration);
+    }
 
-            return result.HasValue ? result.Value : default;
-        }
+    public Task<bool> Set<TValue>(string key, TValue value)
+    {
+        return this.Set(key, value, DateTimeOffset.UtcNow.AddYears(100).ToUnixTimeMilliseconds());
+    }
 
-        public Task<bool> Set<TValue>(string key, TValue value, long issuedAt)
-        {
-            if (issuedAt != 0 && issuedAt < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
-            {
-                this.Delete(key);
-                return Task.FromResult(false);
-            }
-
-            var duration = (int)((issuedAt - DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()) / 1000);
-
-            return this.client.SetAsync(key, value, duration);
-        }
-
-        public Task<bool> Set<TValue>(string key, TValue value)
-        {
-            return this.Set(key, value, DateTimeOffset.UtcNow.AddYears(100).ToUnixTimeMilliseconds());
-        }
-
-        protected override void DisposeManage()
-        {
-            base.DisposeManage();
-            this.client.Dispose();
-        }
+    protected override void DisposeManage()
+    {
+        base.DisposeManage();
+        this.client.Dispose();
     }
 }
