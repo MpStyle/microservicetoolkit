d8d7cba6850c475c0b8ac1c2f5827854b4ad2060
Michele Pagnin <MpStyle@users.noreply.github.com>
2024-07-14 09:29:02 +0200

    Entity Store Manager - Added capability to remove an Item

---
+++ microservice.toolkit.entitystoremanager/entity/service/ItemDelete.cs
@@ -0,0 +1,16 @@
+﻿namespace microservice.toolkit.entitystoremanager.entity.service
+{
+    public class ItemDeleteRequest
+    {
+        public string Id { get; set; }
+        public string ExcludeId { get; set; }
+        public string[] Ids { get; set; }
+
+        public IWhere Filters { get; set; }
+    }
+
+    public class ItemDeleteResponse
+    {
+        public int DeletedRows { get; set; }
+    }
+}
