2790b300ba691d81a57f27e9bfee1617222dabf2
Alex Fontana <alex.fontana.driver@gmail.com>
2024-08-01 13:14:09 +0200

    Added utils method to call ToPattern methods

---
+++ microservice.toolkit.messagemediator/extension/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using microservice.toolkit.core.extension;
+
+namespace microservice.toolkit.messagemediator.extension;
+
+public class ServiceExtensions
+{
+    public static string PatternOf<T>() where T : IService
+    {
+        return typeof(T).ToPattern();
+    }
+}
---
+++ microservice.toolkit.messagemediator/extension/SignalHandlerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using microservice.toolkit.core.extension;
+
+namespace microservice.toolkit.messagemediator.extension;
+
+public class SignalHandlerExtensions
+{
+    public static string PatternOf<T>() where T : ISignalHandler
+    {
+        return typeof(T).ToPattern();
+    }
+}
