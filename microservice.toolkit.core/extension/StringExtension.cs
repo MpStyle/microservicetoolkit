b6db5c020a40a73c6179798c984d4c15f7848af4
Alex Fontana <alex.fontana.driver@gmail.com>
2024-09-05 12:36:30 +0200

    Added IsNotNullOrEmpty in string extensions class

--- microservice.toolkit.core/extension/StringExtension.cs
+++ microservice.toolkit.core/extension/StringExtension.cs
@@ -6,5 +6,10 @@
         {
             return string.IsNullOrEmpty(s);
         }
+        
+        public static bool IsNotNullOrEmpty(this string s)
+        {
+            return !string.IsNullOrEmpty(s);
+        }
     }
 }
