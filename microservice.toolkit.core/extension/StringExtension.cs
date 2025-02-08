ec60b34133201c52733673052db798b5c3437d8e
Michele Pagnin <MpStyle@users.noreply.github.com>
2025-02-08 07:47:56 +0100

    [ENH] Added NotNullWhen attribute to IsNullOrEmpty extension method of StringExtension class

--- microservice.toolkit.core/extension/StringExtension.cs
+++ microservice.toolkit.core/extension/StringExtension.cs
@@ -1,12 +1,14 @@
-﻿namespace microservice.toolkit.core.extension
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace microservice.toolkit.core.extension
 {
     public static class StringExtension
     {
-        public static bool IsNullOrEmpty(this string s)
+        public static bool IsNullOrEmpty([NotNullWhen(false)] this string s)
         {
             return string.IsNullOrEmpty(s);
         }
-        
+
         public static bool IsNotNullOrEmpty(this string s)
         {
             return !string.IsNullOrEmpty(s);
